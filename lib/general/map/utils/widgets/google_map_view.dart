d9165fa84639d84168b5195a508e9edcf24ea22c
anto00n <antonyouhana@gmail.com>
2025-06-09 20:16:28 +0300

    maps

--- lib/general/map/utils/widgets/google_map_view.dart
+++ lib/general/map/utils/widgets/google_map_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
 import 'package:moto/general/map/utils/location_service.dart';
 
 class GoogleMapView extends StatefulWidget {
